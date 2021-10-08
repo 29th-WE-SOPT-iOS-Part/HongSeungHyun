c74cc23a6c25e62cc966abda8c10b6e65a10dae0
Noah <noa316@naver.com>
2021-10-08 20:36:32 +0900

    💄[MOD] : 회원가입 화면 UI 구성하기

---
+++ Assignment/29th-week1-assignment/29th-week1-assignment/SignUpVC.swift
@@ -0,0 +1,28 @@
+//
+//  SignUpVC.swift
+//  29th-week1-assignment
+//
+//  Created by Noah on 2021/10/08.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+
+	@IBOutlet var nameTextField: UITextField!
+	@IBOutlet var emailPhoneTextField: UITextField!
+	@IBOutlet var pwTextField: UITextField!
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+	@IBAction func showPwDidTap(_ sender: UIButton) {
+		sender.isSelected.toggle()
+	}
+	
+	@IBAction func signInButtonDidTap(_ sender: Any) {
+	}
+}
