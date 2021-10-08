610461da4dc56a2c535b1930850258f2d00e6960
Noah <noa316@naver.com>
2021-10-09 02:27:51 +0900

    ✅[CHORE] : 주석 추가

--- Assignment/29th-week1-assignment/29th-week1-assignment/LoginVC.swift
+++ Assignment/29th-week1-assignment/29th-week1-assignment/LoginVC.swift
@@ -20,6 +20,7 @@ class LoginVC: UIViewController {
 		setLoginDescLabel()
 		signInButton.isEnabled = false
 
+		/// 이벤트가 발생했을 때 target 오브젝트와 action 메서드를 이어줍니다.
 		[nameTextField, emailPhoneTextField, pwTextField].forEach {
 			$0?.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
 		}
@@ -34,8 +35,9 @@ class LoginVC: UIViewController {
 
 	@objc
 	func textFieldDidChange(textField: UITextField) {
+		/// 모든 텍스트필드의 값이 비어 있지않다면 true가 됩니다.
 		let allTextFieldsHaveText = [nameTextField, emailPhoneTextField, pwTextField].allSatisfy { $0.text?.isEmpty == false }
-		
+
 		if allTextFieldsHaveText {
 			signInButton.isEnabled = true
 		} else {
@@ -44,15 +46,21 @@ class LoginVC: UIViewController {
 	}
 
 	@IBAction func signInButtonDidTap(_ sender: Any) {
+		/// 새로운 뷰 컨트롤러의 인스턴스를 생성합니다.
 		guard let completeVC = self.storyboard?.instantiateViewController(withIdentifier: "CompleteVC") as? CompleteVC else { return }
 
+		/// nameTextField의 text를 completeVC의 userName에 할당합니다.
 		completeVC.userName = nameTextField.text
+		/// modalPresentationStyle을 전체화면으로 합니다.
 		completeVC.modalPresentationStyle = .fullScreen
+		/// present 메소드를 이용하여 화면을 전환합니다.
 		self.present(completeVC, animated: true)
 	}
 
 	@IBAction func signUpButtonDidTap(_ sender: Any) {
+		/// 새로운 뷰 컨트롤러의 인스턴스를 생성합니다.
 		guard let signUpVC = self.storyboard?.instantiateViewController(withIdentifier: "SignUpVC") else { return }
+		/// 내비게이션 컨트롤러를 사용하여 화면을 전환합니다.
 		self.navigationController?.pushViewController(signUpVC, animated: true)
 	}
 }
