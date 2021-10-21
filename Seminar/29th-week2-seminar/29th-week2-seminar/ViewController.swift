e17af1913f6c46a157ba77012b62ca4e8f551b37
Noah <noa316@naver.com>
2021-10-21 19:34:20 +0900

    ➕[ADD] : 2주차 실습 프로젝트 생성

---
+++ Seminar/29th-week2-seminar/29th-week2-seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  29th-week2-seminar
+//
+//  Created by Noah on 2021/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
