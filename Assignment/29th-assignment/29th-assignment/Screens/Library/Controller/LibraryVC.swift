151c16f83f707ed3e3b1c488fa498c528fe8cced
Noah <noa316@naver.com>
2021-10-22 23:41:46 +0900

    ✨[FEAT] : TabBar에 5개의 view controller 연결하기 - AddVC, HomeVC, LibraryVC, ShortsVC, SubscriptionsVC

---
+++ Assignment/29th-assignment/29th-assignment/Screens/Library/Controller/LibraryVC.swift
@@ -0,0 +1,31 @@
+//
+//  LibraryVC.swift
+//  29th-assignment
+//
+//  Created by Noah on 2021/10/22.
+//
+
+import UIKit
+
+class LibraryVC: UIViewController {
+
+	static let identifier = "LibraryVC"
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
