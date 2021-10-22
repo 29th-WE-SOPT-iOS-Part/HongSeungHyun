bd25f78dd1a5dc76b82ce2703bd0c9b479f1cc13
Noah <noa316@naver.com>
2021-10-22 23:35:44 +0900

    ✨[FEAT] : 확인 버튼 누르면 TabBar로 연결된 화면 present하기 - CompleteVC.swift : 확인 화면에서 확인 버튼 누르면 TabBar로 연결된 화면   present하기

---
+++ Assignment/29th-assignment/29th-assignment/Screens/Sign/Controllers/CompleteVC.swift
@@ -0,0 +1,48 @@
+//
+//  completeVC.swift
+//  29th-assignment
+//
+//  Created by Noah on 2021/10/08.
+//
+
+import UIKit
+
+class CompleteVC: UIViewController {
+
+	@IBOutlet var greetingMessage: UILabel!
+	var userName: String?
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		setGreetingMessage()
+	}
+
+	/// userName 옵셔널 언래핑 + 빈문자열 여부 확인
+	func setUserName() -> String {
+		if let user = userName, user.count > 0 {
+			return user
+		} else {
+			return "회원"
+		}
+	}
+
+	/// 환영 메시지 설정
+	func setGreetingMessage() {
+		let user = setUserName()
+		greetingMessage.text = """
+			\(user)님
+			환영합니다!
+			"""
+		greetingMessage.numberOfLines = 0
+		greetingMessage.textAlignment = .center
+		greetingMessage.sizeToFit()
+	}
+
+	@IBAction func checkButtonDidTap(_ sender: Any) {
+		let tabBarSB = UIStoryboard(name: "TabBar", bundle: nil)
+		guard let tabBarVC = tabBarSB.instantiateViewController(identifier:"TBC") as? TBC else { return }
+
+		tabBarVC.modalPresentationStyle = .fullScreen
+		self.present(tabBarVC, animated: true, completion: nil)
+	}
+}
