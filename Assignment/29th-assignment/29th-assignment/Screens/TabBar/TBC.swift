ad7ef12fa7cb2fb242c4f133a92bf526e0bad7dd
Noah <noa316@naver.com>
2021-10-22 23:43:46 +0900

    ✨[FEAT] : TabBar title, image, selected image 바꾸기

--- Assignment/29th-assignment/29th-assignment/Screens/TabBar/TBC.swift
+++ Assignment/29th-assignment/29th-assignment/Screens/TabBar/TBC.swift
@@ -18,19 +18,34 @@ class TBC: UITabBarController {
 		/// HomeTab
 		let homeSB = UIStoryboard(name: "Home", bundle: nil)
 		guard let homeTab = homeSB.instantiateViewController(withIdentifier: "HomeVC") as? HomeVC else { return }
+		homeTab.title = "홈"
+		homeTab.tabBarItem.image = UIImage(named: "homeIcon")
+		homeTab.tabBarItem.selectedImage = UIImage(named: "homeIconFill")
 
 		/// ShortsTab
 		let shortsSB = UIStoryboard(name: "Shorts", bundle: nil)
 		guard let shortsTab = shortsSB.instantiateViewController(withIdentifier: "ShortsVC") as? ShortsVC else { return }
+		shortsTab.title = "Shorts"
+		shortsTab.tabBarItem.image = UIImage(named: "shortsIcon")
+		shortsTab.tabBarItem.selectedImage = UIImage(named: "shortsIconFill")
 
 		let addSB = UIStoryboard(name: "Add", bundle: nil)
 		guard let addTab = addSB.instantiateViewController(withIdentifier: "AddVC") as? AddVC else { return }
+		addTab.title = "추가"
+		addTab.tabBarItem.image = UIImage(named: "plueCircleIcon")
+		addTab.tabBarItem.selectedImage = UIImage(named: "plueCircleIcon")
 
 		let subscriptionsSB = UIStoryboard(name: "Subscriptions", bundle: nil)
 		guard let subscriptionsTab = subscriptionsSB.instantiateViewController(withIdentifier: "SubscriptionsVC") as? SubscriptionsVC else { return }
+		subscriptionsTab.title = "구독"
+		subscriptionsTab.tabBarItem.image = UIImage(named: "subscriptionsIcon")
+		subscriptionsTab.tabBarItem.selectedImage = UIImage(named: "subscriptionsIconFill")
 
 		let librarySB = UIStoryboard(name: "Library", bundle: nil)
 		guard let libraryTab = librarySB.instantiateViewController(withIdentifier: "LibraryVC") as? LibraryVC else { return }
+		libraryTab.title = "보관함"
+		libraryTab.tabBarItem.image = UIImage(named: "LibraryIcon")
+		libraryTab.tabBarItem.selectedImage = UIImage(named: "LibraryIconFill")
 
 		setViewControllers([homeTab, shortsTab, addTab, subscriptionsTab, libraryTab], animated: false)
 	}
