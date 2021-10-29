6c20ce1ea86e49c55b860c5ea66646637d16d420
Noah <noa316@naver.com>
2021-10-29 19:45:16 +0900

    ➕[ADD] : TableView 관련모델 추가 - HomeManager.swift: Thumbnail 모델과, Channel 모델을 관리해주는   HomeManager 추가 - Thumbnail.swift: TableView 관련 모델 추가

---
+++ Assignment/29th-assignment/29th-assignment/Screens/Home/Model/Thumbnail.swift
@@ -0,0 +1,18 @@
+//
+//  ThumbnailModel.swift
+//  29th-assignment
+//
+//  Created by Noah on 2021/10/29.
+//
+
+import Foundation
+import UIKit
+
+struct Thumbnail  {
+	var thumbNailImage: UIImage
+	var	profileImage: UIImage
+	var title: String
+	var author: String
+	var views: String
+	var createdDate: String
+}
