35eb88d2746601d6aae00eb36a087a1e0de0d51c
Noah <noa316@naver.com>
2021-10-29 19:55:57 +0900

    ✨[FEAT] : 3주차과제 UICollectionView, UITableView 구현

--- Assignment/29th-assignment/29th-assignment/Screens/Home/Controller/HomeVC.swift
+++ Assignment/29th-assignment/29th-assignment/Screens/Home/Controller/HomeVC.swift
@@ -9,21 +9,36 @@ import UIKit
 
 class HomeVC: UIViewController {
 
-    override func viewDidLoad() {
+	var manager = HomeManager.shared
+	
+	@IBOutlet weak var tableView: UITableView! {
+		didSet {
+			tableView.delegate = self
+			tableView.dataSource = self
+			registerTableViewXib()
+		}
+	}
+	
+	@IBOutlet weak var collectionView: UICollectionView! {
+		didSet {
+			collectionView.delegate = self
+			collectionView.dataSource = self
+			registerCollectionViewXib()
+		}
+	}
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	
+	func registerTableViewXib() {
+		let xibName = UINib(nibName: ThumbnailTVC.identifier, bundle: nil)
+		tableView.register(xibName, forCellReuseIdentifier: ThumbnailTVC.identifier)
+	}
+	
+	func registerCollectionViewXib() {
+		let xibName = UINib(nibName: ChannelCVC.identifier, bundle: nil)
+		collectionView.register(xibName, forCellWithReuseIdentifier: ChannelCVC.identifier)
+	}
 }
+
