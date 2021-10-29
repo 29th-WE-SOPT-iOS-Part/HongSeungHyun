58e408c6ec407583567182d3c791f01382e4fe5b
Noah <noa316@naver.com>
2021-10-29 19:47:39 +0900

    ➕[ADD] : UITableView Extension 추가 - HomeVC - HomeVC+TableView.swift : UITableViewDelegate, UITableViewDataSource 구현

---
+++ Assignment/29th-assignment/29th-assignment/Screens/Home/Controller/HomeVC+TableView.swift
@@ -0,0 +1,26 @@
+//
+//  HomeVC+TableView.swift
+//  29th-assignment
+//
+//  Created by Noah on 2021/10/29.
+//
+
+import UIKit
+
+extension HomeVC: UITableViewDelegate {
+	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+		return 306
+	}
+}
+
+extension HomeVC: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return manager.thumbnails.count
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		guard let cell = tableView.dequeueReusableCell(withIdentifier: ThumbnailTVC.identifier) as? ThumbnailTVC else { return UITableViewCell() }
+		cell.getCellConfigureAt(indexPath.row)
+		return cell
+	}
+}
