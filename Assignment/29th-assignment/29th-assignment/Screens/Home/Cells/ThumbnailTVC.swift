631fddb8f4a001777d460eba01d21730d0965cc0
Noah <noa316@naver.com>
2021-10-29 19:42:00 +0900

    ✨[FEAT] : TableViewCell 구현

---
+++ Assignment/29th-assignment/29th-assignment/Screens/Home/Cells/ThumbnailTVC.swift
@@ -0,0 +1,37 @@
+//
+//  ThumbnailTVC.swift
+//  29th-assignment
+//
+//  Created by Noah on 2021/10/29.
+//
+
+import UIKit
+
+class ThumbnailTVC: UITableViewCell {
+
+	static let identifier = "ThumbnailTVC"
+
+	var manager =  HomeManager.shared
+	
+	@IBOutlet weak var thumbnailImage: UIImageView!
+	@IBOutlet weak var profileImage: UIImageView!
+	@IBOutlet weak var thumbnailTitle: UILabel!
+	@IBOutlet weak var infoLabel: UILabel!
+	
+	override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+	func getCellConfigureAt(_ index: Int) {
+		thumbnailImage.image = manager.getThumbnailImage(index)
+		thumbnailTitle.text = manager.getThumbnailTitle(index)
+		let author = manager.getThumbnailAuthor(index)
+		let views = manager.getThumbnailViews(index)
+		let created = manager.getThumbnailCreatedDate(index)
+		infoLabel.text = "\(author) ・ 조회수 \(views) ・ \(created)"
+	}
+}
