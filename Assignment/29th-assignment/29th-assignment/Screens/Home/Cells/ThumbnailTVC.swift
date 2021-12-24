1ad0d6dd97aaa5ae12ed469e473ce2277ccd005e
Noah <noa316@naver.com>
2021-12-24 23:27:49 +0900

    ✨[FEAT] : imageView tap gesture recognizer 연결

--- Assignment/29th-assignment/29th-assignment/Screens/Home/Cells/ThumbnailTVC.swift
+++ Assignment/29th-assignment/29th-assignment/Screens/Home/Cells/ThumbnailTVC.swift
@@ -11,13 +11,14 @@ class ThumbnailTVC: UITableViewCell {
 	
 	var manager =  HomeManager.shared
 	
-	@IBOutlet weak var thumbnailImage: UIImageView!
-	@IBOutlet weak var profileImage: UIImageView!
-	@IBOutlet weak var thumbnailTitle: UILabel!
-	@IBOutlet weak var infoLabel: UILabel!
+	@IBOutlet weak var thumbnailImage: UIImageView?
+	@IBOutlet weak var profileImage: UIImageView?
+	@IBOutlet weak var thumbnailTitle: UILabel?
+	@IBOutlet weak var infoLabel: UILabel?
 	
 	override func awakeFromNib() {
 		super.awakeFromNib()
+		initTapGestureForImageView()
 	}
 	
 	override func setSelected(_ selected: Bool, animated: Bool) {
@@ -30,11 +31,24 @@ extension ThumbnailTVC: CellProtocol {
 	static let identifier = "ThumbnailTVC"
 	
 	func getCellConfigureAt(_ index: Int) {
-		thumbnailImage.image = manager.getThumbnailImage(index)
-		thumbnailTitle.text = manager.getThumbnailTitle(index)
+		thumbnailImage?.image = manager.getThumbnailImage(index)
+		thumbnailTitle?.text = manager.getThumbnailTitle(index)
 		let author = manager.getThumbnailAuthor(index)
 		let views = manager.getThumbnailViews(index)
 		let created = manager.getThumbnailCreatedDate(index)
-		infoLabel.text = "\(author) ・ 조회수 \(views) ・ \(created)"
+		infoLabel?.text = "\(author) ・ 조회수 \(views) ・ \(created)"
+	}
+}
+
+extension ThumbnailTVC {
+	private func initTapGestureForImageView() {
+		let tapRecognizer = UITapGestureRecognizer(target: self,
+												   action: #selector(self.didTapProfileImage))
+		thumbnailImage?.addGestureRecognizer(tapRecognizer)
+		thumbnailImage?.isUserInteractionEnabled = true
+	}
+	
+	@objc func didTapProfileImage() {
+	// doSomething()
 	}
 }
