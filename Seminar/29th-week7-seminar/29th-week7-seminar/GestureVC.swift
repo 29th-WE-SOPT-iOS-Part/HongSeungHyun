019b12f73b93364ec9138e02f81305b222c5e7ab
Noah <noa316@naver.com>
2021-12-24 18:58:51 +0900

    ✨[FEAT] : Gesture Recognizer 추가하기 - 인터페이스 빌더를 통해 추가하기

---
+++ Seminar/29th-week7-seminar/29th-week7-seminar/GestureVC.swift
@@ -0,0 +1,19 @@
+//
+//  GestureVC.swift
+//  29th-week7-seminar
+//
+//  Created by Noah on 2021/12/24.
+//
+
+import UIKit
+
+class GestureVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+	
+	@IBAction func tapGesture(_ sender: UITapGestureRecognizer) {
+		print("uiView 안에서 tap")
+	}
+}
